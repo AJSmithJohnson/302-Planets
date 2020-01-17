e5e3696f7f91e9a91f0b4843ee5617fe67bdf1fc
AJamesSmith <smithja@ferris.edu>
2020-01-16 23:59:47 -0500

    Did some work on space station

--- Assets/Scripts/SpaceStationControls.cs
+++ Assets/Scripts/SpaceStationControls.cs
@@ -11,10 +11,20 @@ public class SpaceStationControls : MonoBehaviour
     public bool flip;
 
     /// <summary>
+    /// Decides if we want to swivel the space station
+    /// </summary>
+    public bool swivel;
+
+    /// <summary>
+    /// do we play the animation forward or backwards
+    /// </summary>
+    public bool isPlayingForward = true;
+
+    /// <summary>
     /// Two rotation values that the SpaceStation can use to swivel along
     /// </summary>
-    public Quaternion a;
-    public Quaternion b;
+    public float a;
+    public float b;
 
     /// <summary>
     /// Floats used to hold information on the angles of the SpaceStation
@@ -28,6 +38,17 @@ public class SpaceStationControls : MonoBehaviour
     /// </summary>
     public float rotateSpeed;
 
+    /// <summary>
+    /// Floats used to keep track of animation 
+    /// </summary>
+    public float animationTime = 1;
+    public float animationTimeCurrent = 0;
+
+    //we use this value to evaluate where the curve is at a specific position
+    [Range(0, 1)] public float percent;
+
+    //creates an animation curve that we can use to store keyframes and other info
+    public AnimationCurve curve;
 
     // Start is called before the first frame update
     void Start()
@@ -40,10 +61,61 @@ public class SpaceStationControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            if(swivel) { swivel = false; }
+            else { swivel = true; }
+        }//End of Keydown W
+
+        
+        if(swivel)
+        {
+            AnimationUpdate();
+        }//end of swivel 
+
+
         //dont need this just yet
-       // AdjustAngles(flip);//We adjust our angles
+        // AdjustAngles(flip);//We adjust our angles
         transform.Rotate(xAngle, yAngle, zAngle, Space.World);// we apply our rotation
     }
+
+    private void AnimationUpdate()
+    {
+        if(isPlayingForward)
+        {
+            animationTimeCurrent += Time.deltaTime;//Tick up our animation time
+            //if (animationTimeCurrent > animationTime) isPlayingForward = false;//If our current animation time gets too high we reverse the boolean and tick down
+        }
+        else
+        {
+            animationTimeCurrent -= Time.deltaTime;
+            //if (animationTimeCurrent < 0) isPlayingForward = true;// if our current animation time gets too low we reverse the boolean and tick up
+        }
+        /*if(animationTimeCurrent > 1)
+        {
+            swivel = false;
+        }*/
+        percent = animationTimeCurrent / animationTime;
+        SwivelPosition();
+    }
+
+    /// <summary>
+    /// Uses lerp to 
+    /// </summary>
+    private void SwivelPosition()
+    {
+        //if (a == null || b == null) return; //These are float values by definition they will have a value
+
+        float p = curve.Evaluate(percent);
+
+        float targetValue = AnimMath.Lerp(a, b, p);
+        xAngle = AnimMath.Dampen(0, targetValue, .5f);
+        
+    }
+
+
+
 
     /// <summary>
     /// This actually sets the rotation of the space station
